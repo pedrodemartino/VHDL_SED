e48be3723a2d0aab5a3639f10f73ccb8682c5ee3
victorfeijoo <100071272+victorfeijoo@users.noreply.github.com>
2022-12-22 10:48:45 +0100

    cambio a 8 bits y mejora del funcionamiento

--- COUNTER.vhd
+++ COUNTER.vhd
@@ -5,28 +5,28 @@ USE ieee.std_logic_arith.all;
 
 entity COUNTER is
 generic(
-		WIDTH : positive :=7 -- de esta manera podemos contar hasta 2 a la 5
+		WIDTH : positive :=8 -- de esta manera podemos contar hasta 2 a la 5
 	);
 	port(
-		MAX: std_logic_vector (WIDTH-1 DOWNTO 0); --Indica hasta cuando hay que contar
+		MAX: std_logic_vector (WIDTH-1 DOWNTO 0):= "00000000"; --Indica hasta cuando hay que contar
 		RESET  : in std_logic; --Reset negado
-		CLK : in std_logic; --Señal de reloj
+		CLKOUT : in std_logic; --Senal de reloj
 		COUT : out std_logic_vector (WIDTH-1 DOWNTO 0)
 	);
 end COUNTER;
 
 architecture E1 of COUNTER is
-    signal CI : std_logic_vector (cout'range) := "0000000"; --Valor inicial para 5 digitos 
+    signal CI : std_logic_vector (cout'range) := "00000000"; --Valor inicial para 5 digitos 
 begin
-    CONTADOR:process(clk, reset)
+    CONTADOR:process(CLKOUT, reset)
         begin
             if RESET = '1' then
                     CI <= (OTHERS => '0');
-            elsif rising_edge(CLK) then
+            elsif rising_edge(CLKOUT) then
                     if	(CI < MAX) then
                         CI <= CI + 1; --el contador aumenta si valor con cada flanco de reloj
-                    elsif (CI = MAX) then
-                        CI <= (others => '0'); --si el contador tiene un valor igual al máximo entonces se resetea
+                    elsif (CI >= MAX) then
+                        CI <= (others => '0'); --si el contador tiene un valor igual al mÃ¡ximo entonces se resetea
                     end if;
             else
                 CI <= CI; --No se si esta parte es necesaria
@@ -35,4 +35,3 @@ end process;
     COUT <= CI;		
 end E1;
 
-end E1;
