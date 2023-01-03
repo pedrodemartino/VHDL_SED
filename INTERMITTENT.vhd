55386eb0f5daec2e2c12fa17eab06069a038fe4f
victorfeijoo <100071272+victorfeijoo@users.noreply.github.com>
2023-01-03 16:46:23 +0100

    eliminación del tiempo como entrada

--- INTERMITTENT.vhd
+++ INTERMITTENT.vhd
@@ -16,11 +16,11 @@ architecture e1 of INTERMITTENT is
 begin
     
 process(clk,tiempo,sal)
-    variable salaux: std_logic_vector (9 downto 0):="0000000000";
+    variable salaux: std_logic_vector (9 downto 0):="1000010100";
 	begin
 		if rising_edge(CLK) then
 			if(sal = "1111111111") then
-			         if tiempo(0) = '0' then
+			         if salaux = "1000010110" then
                         salaux := "1000010100"; -- SP1 rojo, SP2 todo apagado
                     else
                         salaux := "1000010110"; -- SP1 rojo, SP2 verde encendido
