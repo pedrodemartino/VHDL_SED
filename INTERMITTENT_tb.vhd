506e1606e399cdd17a1550bc75a0a78e4acfe7bf
victorfeijoo <100071272+victorfeijoo@users.noreply.github.com>
2023-01-03 16:47:21 +0100

    eliminación del tiempo como entrada

--- INTERMITTENT_tb.vhd
+++ INTERMITTENT_tb.vhd
@@ -9,14 +9,12 @@ architecture A1 of INTERMITTENT_tb is
     component INTERMITTENT
         port(
           clk : in std_logic;
-          tiempo : in std_logic_vector (7 downto 0);
 	      sal    : in std_logic_vector (9 downto 0);
 	      INTERM : out std_logic_vector (9 downto 0)
         );
      end component;
      
      signal clk : std_logic;
-     signal tiempo : std_logic_vector (7 downto 0);
 	 signal sal    : std_logic_vector (9 downto 0);
 	 signal INTERM : std_logic_vector (9 downto 0);
          
@@ -27,7 +25,6 @@ begin
 
     uut:  INTERMITTENT port map(
           clk => CLK,
-          tiempo => tiempo,
 	      sal => SAL,
 	      INTERM => INTERM   
 	      );
@@ -41,23 +38,7 @@ begin
         clk <= '1';
         wait for k / 2;
     end process;
-    
-    
-    
-    tiempo_t: process
-    begin
-        tiempo <= "00000000";
-        wait until rising_edge(clk);
-         tiempo <= "00000001";
-        wait until rising_edge(clk);
-         tiempo <= "00000010";
-        wait until rising_edge(clk);
-         tiempo <= "00000011";
-        wait until rising_edge(clk);
-         tiempo <= "00000100";
-     end process;
-     
-     
+        
      salida_t:process
      begin
          sal <= "1111111111";
