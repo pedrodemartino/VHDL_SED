61aef6201c027aeb0416263a16c83c1afb744a26
victorfeijoo <100071272+victorfeijoo@users.noreply.github.com>
2023-01-10 16:14:34 +0100

    Update INTERMITTENT.vhd

--- INTERMITTENT.vhd
+++ INTERMITTENT.vhd
@@ -5,7 +5,6 @@ use IEEE.STD_LOGIC_ARITH.ALL;
 entity INTERMITTENT is
 port (
     clk : in std_logic;
-    tiempo : in std_logic_vector (7 downto 0);
 	sal    : in std_logic_vector (9 downto 0);
 	INTERM : out std_logic_vector (9 downto 0)
 );
@@ -15,8 +14,10 @@ architecture e1 of INTERMITTENT is
 	--signal salaux: std_logic_vector (9 downto 0);
 begin
     
-process(clk,tiempo,sal)
+process(clk,sal)
     variable salaux: std_logic_vector (9 downto 0):="1000010100";
+    variable salaux2: std_logic_vector (9 downto 0):="0011001001";
+    variable salaux3: std_logic_vector (9 downto 0):="0000000000";
 	begin
 		if rising_edge(CLK) then
 			if(sal = "1111111111") then
@@ -25,10 +26,19 @@ process(clk,tiempo,sal)
                     else
                         salaux := "1000010110"; -- SP1 rojo, SP2 verde encendido
                     end if;
+                    INTERM <= salaux;
+             elsif(sal = "1111111110") then
+                    if salaux2 = "0011001001" then
+                        salaux2 := "0011000001"; -- SP1 rojo, SP2 todo apagado
+                        
+                    else
+                        salaux2 := "0011001001"; -- SP1 rojo, SP2 verde encendido
+                    end if;
+                    INTERM <= salaux2;
 			 else
-	           salaux := sal;	
-	         end if;
-	       INTERM <= salaux;      	  
+	           salaux3 := sal;
+	           INTERM <= salaux3;
+	         end if;  	  
 		end if;		
 	end process;
 end e1;
